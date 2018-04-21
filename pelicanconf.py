2e89072ae24612ee90f087745c78ff8c323c8ac2
Su Henian <su.henian@jiinii.com>
2018-04-21 16:50:12 +0800

    Use theme Flex.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'su.henian'
-SITENAME = 'idiip'
+AUTHOR = 'Su Henian'
+SITENAME = 'idiip site'
 SITEURL = ''
 
 PATH = 'content'
@@ -20,16 +20,27 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+THEME = 'Flex'
+SITETITLE = 'idiip'
+SITESUBTITLE = 'I did it in Python.'
+SITELOGO = '/images/me.png'
+
+MAIN_MENU = True
+
+COPYRIGHT_NAME = 'Jiinii.com'
+COPYRIGHT_YEAR = 2018
+
