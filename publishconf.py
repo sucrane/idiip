9522502af5f050fde19cfc001164857e46bc5da9
Su Henian <su.henian@jiinii.com>
2018-04-22 21:31:01 +0800

    Add google adsense.

--- publishconf.py
+++ publishconf.py
@@ -14,3 +14,16 @@ OUTPUT_PATH = 'docs/'
 SITEURL = 'http://www.jiinii.com/idiip'
 
 GOOGLE_ANALYTICS = "UA-117999375-1"
+
+GOOGLE_ADSENSE = {
+    'ca_id': 'ca-pub-8599880473432439',     # Your AdSense ID
+    'page_level_ads': True,                 # Allow Page Level Ads (mobile)
+    'ads': {
+    #     'aside': '1234561',               # Side bar banner (all pages)
+    #     'main_menu': '1234562',           # Banner before main menu (all pages)
+    #     'index_top': '1234563',           # Banner after main menu (index only)
+    #     'index_bottom': '1234564',        # Banner before footer (index only)
+    #     'article_top': '1234565',         # Banner after article title (article only)
+    #     'article_bottom': '1234566',      # Banner after article content (article only)
+    }
+}
