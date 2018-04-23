1ba6fe39dae43f4d3b5d9e698eee231f9a5a3e70
Su Henian <su.henian@jiinii.com>
2018-04-23 16:27:53 +0800

    Add Google adsense ad slot.

--- publishconf.py
+++ publishconf.py
@@ -19,7 +19,7 @@ GOOGLE_ADSENSE = {
     'ca_id': 'ca-pub-8599880473432439',     # Your AdSense ID
     'page_level_ads': True,                 # Allow Page Level Ads (mobile)
     'ads': {
-    #     'aside': '1234561',               # Side bar banner (all pages)
+        'aside': '4272745832',               # Side bar banner (all pages)
     #     'main_menu': '1234562',           # Banner before main menu (all pages)
     #     'index_top': '1234563',           # Banner after main menu (index only)
     #     'index_bottom': '1234564',        # Banner before footer (index only)
